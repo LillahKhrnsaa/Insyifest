ecfa5429b96b2a473744523c670e906de76dee3f
reihanKhadafi <reihankhadafi202@gmail.com>
2025-10-13 12:40:44 +0700

    Fix(Seeder) : Fix bank account

--- database/seeders/BankAccountSeeder.php
+++ database/seeders/BankAccountSeeder.php
@@ -12,8 +12,24 @@ class BankAccountSeeder extends Seeder
         DB::table('bank_accounts')->insert([
             [
                 'bank_name' => 'Bank Central Asia (BCA)',
-                'account_number' => '1234567890',
-                'account_holder' => 'Andi Wijaya',
+                'account_number' => '3781449341',
+                'account_holder' => 'Moh Luthfi Adistira',
+                'is_active' => true,
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'bank_name' => 'Bank Rakyat Indonesia (BRI)',
+                'account_number' => '326001028392539',
+                'account_holder' => 'Moh Luthfi Adistira',
+                'is_active' => true,
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'bank_name' => 'Bank Rakyat Indonesia (BRI)',
+                'account_number' => '326001000006650',
+                'account_holder' => 'Moh Luthfi Adistira',
                 'is_active' => true,
                 'created_at' => now(),
                 'updated_at' => now(),
