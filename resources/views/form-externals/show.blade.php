fac65b1cef12af789c6a8679e9aadcf7ebd447aa
reihanKhadafiB <reihankhadafi202@gmail.com>
2026-01-26 03:11:50 +0700

    Refactor(form-externals/show) : Change import tailwind become track vite

--- resources/views/form-externals/show.blade.php
+++ resources/views/form-externals/show.blade.php
@@ -4,8 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>{{ $form->title }}</title>
-    {{-- Pastikan Anda sudah memanggil Tailwind CSS dan Alpine.js di sini atau di layout utama --}}
-    <script src="https://cdn.tailwindcss.com"></script>
+    @vite('resources/css/app.css')
     <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
 </head>
 <body class="bg-slate-100">
