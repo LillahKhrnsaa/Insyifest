b25f32ccaed05e1c56e7cfb771f7e4c2cbb7608b
reihanKhadafiB <reihankhadafi202@gmail.com>
2026-04-25 15:49:43 +0700

    Feat(Resources) : add password fields, unique phone validation, and success modal with PDF download to member registration

--- app/Filament/Resources/PengaturanAdmins/Tables/PengaturanAdminsTable.php
+++ app/Filament/Resources/PengaturanAdmins/Tables/PengaturanAdminsTable.php
@@ -4,6 +4,11 @@ namespace App\Filament\Resources\PengaturanAdmins\Tables;
 
 use Filament\Tables\Columns\TextColumn;
 use Filament\Tables\Table;
+use Filament\Actions\EditAction;
+use Filament\Actions\DeleteAction;
+use Filament\Actions\ActionGroup;
+use Filament\Actions\BulkActionGroup;
+use Filament\Actions\DeleteBulkAction;
 
 class PengaturanAdminsTable
 {
@@ -51,6 +56,42 @@ class PengaturanAdminsTable
                     ->alignCenter()
                     ->color(fn ($state, $record) => $state >= $record->quota ? 'danger' : 'success'),
             ])
-            ->defaultSort('coach_id');
+            ->defaultSort('coach_id')
+            ->recordActions([
+                ActionGroup::make([
+                    EditAction::make()
+                        ->label('Edit')
+                        ->tooltip('Edit data')
+                        ->icon('heroicon-o-pencil-square')
+                        ->color('primary')
+                        ->size('sm')
+                        ->extraAttributes([
+                            'class' => 'border border-blue-300 text-blue-700 bg-white hover:bg-blue-50 rounded-lg px-3 py-2'
+                        ]),
+
+                    DeleteAction::make()
+                        ->label('Hapus')
+                        ->tooltip('Hapus data')
+                        ->icon('heroicon-o-trash')
+                        ->color('danger')
+                        ->size('sm')
+                        ->requiresConfirmation()
+                        ->modalHeading('Hapus Data')
+                        ->modalDescription('Yakin ingin menghapus data ini?')
+                        ->modalSubmitActionLabel('Hapus')
+                        ->modalCancelActionLabel('Batal')
+                        ->extraAttributes([
+                            'class' => 'border border-red-300 text-red-700 bg-white hover:bg-red-50 rounded-lg px-3 py-2'
+                        ]),
+                ])
+                ->icon('heroicon-o-bars-4')
+                ->label('')
+                ->button()
+            ])
+            ->bulkActions([
+                BulkActionGroup::make([
+                    DeleteBulkAction::make(),
+                ]),
+            ]);
     }
 }
